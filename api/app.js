3b63521f8a1bb54205a46cf663bec803f15c16f4
Zubair <zubairharitage@gmail.com>
2023-02-12 21:13:46 +0500

    initial

--- api/app.js
+++ api/app.js
@@ -1,11 +1,11 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 
 import route from "./routes/billRoute.js";
 import userRoute from "./routes/userRoute.js";
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
